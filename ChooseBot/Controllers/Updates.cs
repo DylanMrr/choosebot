9544b1d2eeefa8dfbeff30aff75d5f6f8f98dd16
OSINIT\dmitriy.eginov <dmitriy.eginov@osinit.com>
2021-02-15 10:16:12 +0300

    first

---
+++ ChooseBot/Controllers/Updates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ChooseBot.Controllers
+{
+    [Serializable]
+    public class Updates
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+    }
+}
---
+++ ChooseBot/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace ChooseBot
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddSingleton<IVkApi>(sp =>
+            {
+                var vkApi = new VkApi();
+                vkApi.Authorize(new ApiAuthParams { AccessToken = Configuration["Config:AccessToken"] });
+                return vkApi;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
